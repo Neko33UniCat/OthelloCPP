9e3edcecef115ef1d4b88ee81ca90afdc0fc2379
Hiro <luckyhiroaki@icloud.com>
2020-02-02 16:22:08 +0900

    add ArchiveCommand

--- main.cpp
+++ main.cpp
@@ -1,32 +1,1 @@
-#include <iostream>
-#include "include/BitBoard.hpp"
-#include "include/Functions.hpp"
-using namespace std;
-
-int main()
-{
-    int n;
-    BitBoard bb;
-    bb.Score();
-    bb.Show();
-    while(true){
-        if((~(bb.Black | bb.White)) == (u_long)0) {
-            break;
-        }
-        if(bb.canOnBlack()!=0){
-            cout<<"TurnBlack"<<endl;
-            cin >> n;
-            bb.ReverseTurnBlack(n);
-            bb.Score();
-            bb.Show();
-        }
-        if(bb.canOnWhite()!=0){
-            cout<<"TurnWhite"<<endl;
-            cin >> n;
-            bb.ReverseTurnWhite(n);
-            bb.Score();
-            bb.Show();
-        }
-    }
-    return 0;
-}                       
+#include <iostream>#include "include/BitBoard.hpp"#include "include/Functions.hpp"using namespace std;int main(){    int n;    BitBoard bb;    bb.Score();    bb.Show();    while(true){        if((~(bb.Black | bb.White)) == (u_long)0) {            break;        }        if(bb.canOnBlack()!=0){            cout<<"TurnBlack"<<endl;            cin >> n;            bb.ReverseTurnBlack(n);            bb.Score();            bb.Show();        }        if(bb.canOnWhite()!=0){            cout<<"TurnWhite"<<endl;            cin >> n;            bb.ReverseTurnWhite(n);            bb.Score();            bb.Show();        }    }    return 0;}                       
